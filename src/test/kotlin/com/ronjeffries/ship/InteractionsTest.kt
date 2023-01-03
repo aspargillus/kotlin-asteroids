ef6321c2283fcecd8c865852021d1b1b92cfdc3b
Frank Schimmel <frank@ueberban.de>
2023-01-03 23:41:01 +0100

    Cleanup test to improve readability.

--- src/test/kotlin/com/ronjeffries/ship/InteractionsTest.kt
+++ src/test/kotlin/com/ronjeffries/ship/InteractionsTest.kt
@@ -6,36 +6,8 @@ import org.junit.jupiter.api.Test
 class InteractionsTest {
     @Test
     fun `beginInteractions should notify all objects`() {
-        val object1 = object : ISpaceObject {
-            var beforeInteractionsCalled: Boolean = false
-
-            override fun update(deltaTime: Double, trans: Transaction) {
-                TODO("Do not call this!")
-            }
-
-            override val subscriptions: Subscriptions = Subscriptions(
-                beforeInteractions = { beforeInteractionsCalled = true }
-            )
-
-            override fun callOther(other: InteractingSpaceObject, trans: Transaction) {
-                TODO("Not yet implemented")
-            }
-        }
-        val object2 = object : ISpaceObject {
-            var beforeInteractionsCalled: Boolean = false
-
-            override fun update(deltaTime: Double, trans: Transaction) {
-                TODO("Do not call this!")
-            }
-
-            override val subscriptions: Subscriptions = Subscriptions(
-                beforeInteractions = { beforeInteractionsCalled = true }
-            )
-
-            override fun callOther(other: InteractingSpaceObject, trans: Transaction) {
-                TODO("Not yet implemented")
-            }
-        }
+        val object1 = InteractionTestObject()
+        val object2 = InteractionTestObject()
         val knownObjects = SpaceObjectCollection().also { it.addAll(listOf(object1, object2)) }
 
         val interactions = Interactions(knownObjects)
@@ -43,6 +15,22 @@ class InteractionsTest {
 
         assertThat(object1.beforeInteractionsCalled).isTrue
         assertThat(object2.beforeInteractionsCalled).isTrue
+    }
+}
+
+class InteractionTestObject : ISpaceObject {
+    var beforeInteractionsCalled: Boolean = false
+
+    override fun update(deltaTime: Double, trans: Transaction) {
+        TODO("Do not call this!")
+    }
+
+    override val subscriptions: Subscriptions = Subscriptions(
+        beforeInteractions = { beforeInteractionsCalled = true }
+    )
+
+    override fun callOther(other: InteractingSpaceObject, trans: Transaction) {
+        TODO("Not yet implemented")
     }
 }
 
