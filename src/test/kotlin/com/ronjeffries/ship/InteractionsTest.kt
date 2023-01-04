2f29763f14e34546b8e8a2e36f0183943eb04788
Frank Schimmel <frank@ueberban.de>
2023-01-04 09:23:33 +0100

    Register and look up lambdas for interacting objects of a combination of classes.

--- src/test/kotlin/com/ronjeffries/ship/InteractionsTest.kt
+++ src/test/kotlin/com/ronjeffries/ship/InteractionsTest.kt
@@ -2,6 +2,9 @@ package com.ronjeffries.ship
 
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.reflect.KClass
+
+typealias Interaction = (object1: ISpaceObject, object2: ISpaceObject, transaction: Transaction) -> Unit
 
 class InteractionsTest {
     @Test
@@ -15,6 +18,30 @@ class InteractionsTest {
 
         assertThat(object1.beforeInteractionsCalled).isTrue
         assertThat(object2.beforeInteractionsCalled).isTrue
+    }
+
+    @Test
+    fun `interaction lookup yields null by default`() {
+        val object1 = InteractionTestObject()
+        val object2 = InteractionTestObject()
+        val knownObjects = SpaceObjectCollection().also { it.addAll(listOf(object1, object2)) }
+
+        val interactions = Interactions(knownObjects)
+
+        assertThat(interactions.findInteraction(object1, object2)).isNull()
+    }
+
+    @Test
+    fun `interaction lookup yields previously registered lambda`() {
+        val object1 = InteractionTestObject()
+        val object2 = InteractionTestObject()
+        val knownObjects = SpaceObjectCollection().also { it.addAll(listOf(object1, object2)) }
+
+        val interactions = Interactions(knownObjects)
+        val interaction = { _: ISpaceObject, _: ISpaceObject, _: Transaction -> }
+        interactions.register(InteractionTestObject::class, InteractionTestObject::class, interaction)
+
+        assertThat(interactions.findInteraction(object1, object2)).isSameAs(interaction)
     }
 }
 
@@ -35,7 +62,18 @@ class InteractionTestObject : ISpaceObject {
 }
 
 class Interactions(private val knownObjects: SpaceObjectCollection) {
+    private val registeredInteractions =
+        mutableMapOf<Pair<KClass<out ISpaceObject>, KClass<out ISpaceObject>>, Interaction>()
+
     fun beginInteractions() {
         knownObjects.forEach { it.subscriptions.beforeInteractions() }
+    }
+
+    fun findInteraction(object1: ISpaceObject,object2: ISpaceObject): (Interaction)? {
+        return registeredInteractions[Pair(object1::class, object2::class)]
+    }
+
+    fun register(class1: KClass<out ISpaceObject>, class2: KClass<out ISpaceObject>, interaction: Interaction) {
+        registeredInteractions[Pair(class1, class2)] = interaction
     }
 }
